66601563e57e2eede97aababf737fd42b3184cff
Arnaud Jeannin <arnaudjeannin@outlook.com>
2020-11-19 12:39:38 +0100

    add auth providers

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -7,36 +7,61 @@ import IUser from "types/user";
 import iToken from "types/token";
 
 const jwtSecret = JSON.parse(process.env.AUTH_PRIVATE_KEY);
-const environment = process.env.NODE_ENV;
-const isProduction = environment === 'production';
-let database = {
-  type: process.env.NEXT_PUBLIC_DATABASE_TYPE,
-  host: process.env.NEXT_PUBLIC_DATABASE_HOST,
-  port: process.env.NEXT_PUBLIC_DATABASE_PORT,
-  username: process.env.NEXT_PUBLIC_DATABASE_USERNAME,
-  password: process.env.NEXT_PUBLIC_DATABASE_PASSWORD,
-  database: process.env.NEXT_PUBLIC_DATABASE_NAME,
-}
-if (!isProduction) {
-  database = {
-    ...database,
-    ssl: true,
-    extra: {
-      ssl: {
-        rejectUnauthorized: false
-      },
-    }
-  }
-}
 
 const options = {
+  // https://next-auth.js.org/configuration/providers
   providers: [
-    Providers.Facebook({
-      clientId: process.env.FACEBOOK_CLIENT_ID,
-      clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+    Providers.Email({
+      server: process.env.EMAIL_SERVER, 
+      from: process.env.EMAIL_FROM,
     }),
+    // Providers.Apple({
+    //   clientId: process.env.APPLE_ID,
+    //   clientSecret: { 
+    //     appleId: process.env.APPLE_ID,
+    //     teamId: process.env.APPLE_TEAM_ID,
+    //     privateKey: process.env.APPLE_PRIVATE_KEY,
+    //     keyId: process.env.APPLE_KEY_ID,
+    //   }
+    // }),
+    // Providers.Auth0({
+    //   clientId: process.env.AUTH0_ID,
+    //   clientSecret: process.env.AUTH0_SECRET,
+    //   domain: process.env.AUTH0_DOMAIN
+    // }),
+    // Providers.Facebook({
+    //   clientId: process.env.FACEBOOK_ID,
+    //   clientSecret: process.env.FACEBOOK_SECRET
+    // }),
+    // Providers.GitHub({
+    //   clientId: process.env.GITHUB_ID,
+    //   clientSecret: process.env.GITHUB_SECRET
+    // }),
+    Providers.Google({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET
+    }),
+    // Providers.Twitter({
+    //   clientId: process.env.TWITTER_ID,
+    //   clientSecret: process.env.TWITTER_SECRET
+    // }),
   ],
-  database,
+  database: {
+    type: 'postgres',
+    host: process.env.DATABASE_HOST,
+    port: process.env.DATABASE_PORT,
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    ...(process.env.NODE_ENV !== 'production') && {
+      ssl: true,
+      extra: {
+        ssl: {
+          rejectUnauthorized: false
+        },
+      }
+    }
+  },
   session: {
     jwt: true,
   },
